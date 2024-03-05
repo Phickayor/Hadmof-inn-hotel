9dbaa2ddc851d43ae988bd9199f4d454c8dc801f
Phickayor <jetawof@gmail.com>
2024-03-05 19:50:00 +0100

    anding page, login, signup ui

--- client/components/auth/Login.tsx
+++ client/components/auth/Login.tsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="relative min-h-screen px-5 md:px-20">
+      <div className="flex [&>*]:self-center md:py-10 py-6 justify-between gap-10">
+        <h1 className="font-semibold md:text-3xl text-xl">Hotel Site</h1>
+      </div>
+      <form className="bg-white mx-auto w-full md:w-8/12 p-4 md:p-10 py-6 md:py-10 rounded-lg my-20 md:my-32">
+        <h3 className="font-semibold inline-block md:text-3xl text-xl border-b-2 border-blue-500 py-1">
+          Login
+        </h3>
+        <div className="flex flex-col md:gap-8 gap-5 md:pt-10 pt-5">
+          <div className="flex flex-col">
+            <label>
+              Email<span className="text-red-500">&nbsp;*</span>
+            </label>
+            <input
+              type="email"
+              required
+              className="w-full border-b-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>
+              Password<span className="text-red-500">&nbsp;*</span>
+            </label>
+            <input
+              type="password"
+              required
+              className="w-full border-b-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button className="self-center bg-blue-500 cursor-pointer text-white rounded-sm py-2 px-8">
+            Register
+          </button>
+        </div>
+        <p className="pt-6">
+          Don&apos;t have an account?
+          <Link href="/auth/register" className="font-semibold text-blue-500">
+            &nbsp;Register
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
--- client/components/auth/Register.tsx
+++ client/components/auth/Register.tsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="relative min-h-screen px-5 md:px-20">
+      <div className="flex [&>*]:self-center md:py-10 py-6 justify-between gap-10">
+        <h1 className="font-semibold md:text-3xl text-xl">Hotel Site</h1>
+      </div>
+      <form className="bg-white mx-auto w-full md:w-8/12 p-4 md:p-10 py-6 md:py-10 rounded-lg my-20">
+        <h3 className="font-semibold inline-block md:text-3xl text-xl border-b-2 border-blue-500 py-1">
+          Register
+        </h3>
+        <div className="flex flex-col md:gap-8 gap-5 md:pt-10 pt-5">
+          <div className="flex flex-col">
+            <label>
+              Username<span className="text-red-500">&nbsp;*</span>
+            </label>
+            <input
+              type="email"
+              required
+              className="w-full border-b-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>
+              Email<span className="text-red-500">&nbsp;*</span>
+            </label>
+            <input
+              type="email"
+              required
+              className="w-full border-b-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label>
+              Password<span className="text-red-500">&nbsp;*</span>
+            </label>
+            <input
+              type="password"
+              required
+              className="w-full border-b-2 focus:outline-none focus:border-blue-500"
+            />
+          </div>
+          <button className="self-center bg-blue-500 cursor-pointer text-white rounded-sm py-2 px-8">
+            Register
+          </button>
+        </div>
+        <p className="pt-6">
+          Already have an account?
+          <Link href="/auth" className="font-semibold text-blue-500">
+            &nbsp;Login
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
