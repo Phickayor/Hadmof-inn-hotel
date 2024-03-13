b95b135cf27e28540b7669d3250da68f350571fd
Phickayor <jetawof@gmail.com>
2024-03-13 07:32:34 +0100

    Compulsory live url

--- client/config/server.config.ts
+++ client/config/server.config.ts
@@ -1,4 +1,4 @@
-export const serverUrl =
-  process.env.NEXT_PUBLIC_ENVIROMENT === "production"
-    ? process.env.NEXT_PUBLIC_LIVE_URL
-    : process.env.NEXT_PUBLIC_LOCAL_URL;
+export const serverUrl = "https://hotel-site-template.onrender.com";
+// process.env.NEXT_PUBLIC_ENVIROMENT === "production"
+//   ? process.env.NEXT_PUBLIC_LIVE_URL
+//   : process.env.NEXT_PUBLIC_LOCAL_URL;
